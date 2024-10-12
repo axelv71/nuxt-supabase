f459efbcdbf27ff05fe5db64de66929f5bfb67a0
Axel <axel.vionyt@gmail.com>
2024-10-12 17:40:43 +0200

    Add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {isBrowser} from "@supabase/ssr";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   typescript: {
     typeCheck: true
   },
-  modules: ['@nuxtjs/tailwindcss'],
+  supabase: {
+    redirectOptions: {
+      login: '/login',
+      callback: '/callback',
+      include: undefined,
+      exclude: ['/register', '/'],
+      cookieRedirect: false,
+    },
+    clientOptions: {
+      auth: {
+        flowType: 'pkce',
+        autoRefreshToken: isBrowser(),
+        detectSessionInUrl: isBrowser(),
+        persistSession: true,
+      },
+    }
+  },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
 })
